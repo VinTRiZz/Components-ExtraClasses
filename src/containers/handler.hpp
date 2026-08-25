d3aeffe81b8933f260920c5381d0c7df6f1676ac
VinTRiZz <andzig501@gmail.com>
2026-08-25 22:03:26 +0300

    Containers@Handler#fix: isValid check issues

--- src/containers/handler.hpp
+++ src/containers/handler.hpp
@@ -26,12 +26,8 @@ public:
     Handler() = default;
     Handler(Handler&& _ohdl) = default;
     Handler(const Handler& _ohdl) = default;
-    explicit Handler(value_t* pTarget) :
-        m_pTarget { std::make_shared<value_t*>(pTarget) }
-    { }
-    explicit Handler(value_t& pTarget) :
-        m_pTarget { std::make_shared<value_t*>(pTarget) }
-    { }
+    explicit Handler(value_t* pTarget) { setPointer(pTarget); }
+    explicit Handler(value_t& pTarget) { setPointer(&pTarget); }
     ~Handler() = default;
 
     // Analog of std::static_pointer_cast
@@ -40,55 +36,55 @@ public:
         static_assert(std::is_base_of_v<value_t, DerivedT> || std::is_base_of_v<DerivedT, value_t>,
                       "Derived class must be related to handler type");
         checkValid();
-        return static_cast<DerivedT*>(*m_pTarget);
+        return static_cast<DerivedT*>(m_pTarget);
     }
     template <typename DerivedT>
     const DerivedT* cast() const noexcept(false) {
         static_assert(std::is_base_of_v<value_t, DerivedT> || std::is_base_of_v<DerivedT, value_t>,
                       "Derived class must be related to handler type");
         checkValid();
-        return static_cast<const DerivedT*>(*m_pTarget);
+        return static_cast<const DerivedT*>(m_pTarget);
     }
 
     // Analog of std::dynamic_pointer_cast
     template <typename DerivedT>
     DerivedT* cast_dynamic() noexcept(false) {
         checkValid();
-        return dynamic_cast<DerivedT*>(*m_pTarget);
+        return dynamic_cast<DerivedT*>(m_pTarget);
     }
     template <typename DerivedT>
     const DerivedT* cast_dynamic() const noexcept(false) {
         checkValid();
-        return dynamic_cast<const DerivedT*>(*m_pTarget);
+        return dynamic_cast<const DerivedT*>(m_pTarget);
     }
 
     // Pointer access operators
     value_t* operator->() noexcept(false) {
         checkValid();
-        return *m_pTarget;
+        return m_pTarget;
     }
     const value_t* operator->() const noexcept(false) {
         checkValid();
-        return *m_pTarget;
+        return m_pTarget;
     }
 
     value_t& operator*() noexcept(false) {
         checkValid();
-        return *(*m_pTarget);
+        return *m_pTarget;
     }
     const value_t& operator*() const noexcept(false) {
         checkValid();
-        return *(*m_pTarget);
+        return *m_pTarget;
     }
 
     // Copy of STL smart pointers logic
-    value_t* get() noexcept { return *m_pTarget; }
-    const value_t* get() const noexcept { return *m_pTarget; }
+    value_t* get() noexcept { return isValid() ? m_pTarget : nullptr; }
+    const value_t* get() const noexcept { return isValid() ? m_pTarget : nullptr; }
     explicit operator bool() const { return isValid(); }
 
     // Handler source management
-    bool isValid() const noexcept { return (nullptr != *m_pTarget); }
-    void invalidate() { *m_pTarget = nullptr; }
+    bool isValid() const noexcept { return *m_isValid; }
+    void invalidate() { setPointer(nullptr); }
 
     // For std::set and others using. Default is pointer address sort
     bool operator <(const Handler& _ohdl) const noexcept {
@@ -96,7 +92,7 @@ public:
             return isValid();
         }
         if constexpr (is_lessComparable<ValueT>::value) {
-            return (*(*m_pTarget) < *(*_ohdl.m_pTarget));
+            return (*m_pTarget < *_ohdl.m_pTarget);
         } else {
             return (m_pTarget < _ohdl.m_pTarget);
         }
@@ -104,19 +100,26 @@ public:
 
     // Comparing
     bool operator ==(value_t* _oval) const noexcept {
-        return isValid() && (*m_pTarget == _oval);
+        return isValid() && (m_pTarget == _oval);
     }
     bool operator ==(const Handler& _ohdl) const noexcept {
-        return (m_pTarget == _ohdl.m_pTarget);
+        return isValid() && _ohdl.isValid() && (m_pTarget == _ohdl.m_pTarget);
     }
 
     // Assigment
     Handler& operator =(const Handler& _ohdl) noexcept = default;
     Handler& operator =(Handler&& _ohdl) noexcept = default;
-    Handler& operator =(value_t* _ovalue) noexcept { *m_pTarget = _ovalue; return *this; }
+    Handler& operator =(value_t* _ovalue) noexcept { setPointer(_ovalue); return *this; }
 
 private:
-    std::shared_ptr<value_t*> m_pTarget { std::make_shared<value_t*>(nullptr) };
+    value_t* m_pTarget {nullptr};
+    std::shared_ptr<bool> m_isValid { std::make_shared<bool>(false) };
+
+    void setPointer(value_t* pTarget) {
+        *m_isValid = false;
+        m_pTarget = pTarget;
+        *m_isValid = (nullptr != m_pTarget);
+    }
 
     void checkValid() const noexcept(false) {
         if (!isValid()) {
